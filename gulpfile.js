11c5159aca419fc9df7bee1ab65d0cc9b777b90f
Aleksander Zhuravlev <azhur@bekitzur.com>
2015-12-24 16:40:19 +0300

    gulp watcher

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 
-gulp.task('compressJS', function() {
+gulp.task('uglify', function() {
   return gulp.src('app/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
@@ -14,9 +14,15 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('makeDist', ['compressJS', 'sass'], function() {
+gulp.task('makeDist', ['uglify', 'sass'], function() {
   return gulp.src('app/**/*.html')
     .pipe(gulp.dest('dist'));
+});
+
+var watcher = gulp.watch(['app/**/*.js','app/**/*.scss','app/**/*.html'], ['uglify']);
+
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 });
 
 gulp.task('default', ['makeDist']);
