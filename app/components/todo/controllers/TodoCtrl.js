c1cb07f028352ca480f9853f9da3b84d4c03fb82
Aleksander Zhuravlev <azhur@bekitzur.com>
2015-12-25 16:26:37 +0300

    edit title and content

--- app/components/todo/controllers/TodoCtrl.js
+++ app/components/todo/controllers/TodoCtrl.js
@@ -3,50 +3,100 @@
 var TodoCtrl = function($scope, $rootScope, $state, $stateParams, $timeout, $mdSidenav) {
 	'ngInject';
 	
-	console.log('TodoCtrl');
-	
-	$scope.openLeftMenu = function() {
-		$mdSidenav('left').toggle();
-	};
-	
+	/**
+	 * Init models
+	 */
 	$scope.models = {
 		selected: null,
 		todoLists: [],
 		allowedTypes: ['list'],
 	};
+	
+	/**
+	 * Open sidebar
+	 */
+	$scope.openLeftMenu = function() {
+		$mdSidenav('left').toggle();
+	};
 
+	/**
+	 * Add new TODO list
+	 */
 	$scope.addTodoList = function() {
 		$scope.models.todoLists.push(
 			{
 				label: 'New todo list (' + $scope.models.todoLists.length + ')',
 				type: 'list',
 				allowedTypes: ['item'],
-				items: [{label: 'item 1', type: 'item'}, {label: 'item 2', type: 'item'}]
+				items: []
 			}
 		);
 	};
 	
+	/**
+	 * Clear all TODO lists
+	 */
 	$scope.clearAllTodoList = function() {
 		$scope.models.todoLists = [];
 	};
 	
+	/**
+	 * Add new row in TODO list
+	 */
 	$scope.addNewRow = function(list) {
-		list.items.push({label: 'new row', type: 'item'});
+		list.items.push({
+			label: 'New row',
+			type: 'item',
+			content: 'please add description...'}
+		);
 	};
 	
+	/**
+	 * Clear all rows in TODO list
+	 */
 	$scope.clearList = function(list) {
 		list.items = [];
 	};
 	
+	/**
+	 * Edit label TODO list
+	 */
 	$scope.editLabel  = function(list) {
 		list.editLabel = true;
 	};
 	
+	/**
+	 * Cancel editing label TODO list
+	 */
+	$scope.saveLabel  = function(list) {
+		list.editLabel = false;
+	};
+	
+	/**
+	 * Edit description TODO item
+	 */
+	$scope.editContent  = function(item) {
+		item.editContent = true;
+	};
+	
+	/**
+	 * Cancel editing description TODO item
+	 */
+	$scope.saveContent  = function(item) {
+		item.editContent = false;
+	};
+	
+	/**
+	 * Cancel editing label TODO list
+	 */
 	$scope.saveLabel  = function(list) {
 		list.label = list.label;
 		list.editLabel = false;
 	};
 	
+	/**
+	 * Open menu of list
+	 */
 	$scope.openMenu = function($mdOpenMenu, ev) {
 		$mdOpenMenu(ev);
 	};
